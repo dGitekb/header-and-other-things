e77b7d5cea8ef641ec9ffb729cfda8c8922413fe
Grzegorz Sztwiertnia <grzegorz9701@gmail.com>
2021-05-11 12:55:56 +0200

    animations
    add animations to menu and create hamburger button

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
-const hamburger = document.querySelector(".toggle-btn");
+const hamburger = document.querySelector(".toggle-button");
 const navigation = document.querySelector(".nav-menu");
+const first = document.querySelector(".first-line");
+const second = document.querySelector(".second-line");
+const third = document.querySelector(".thirde-line");
+const about = document.querySelector(".about-me");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navigation.classList.toggle("active");
+    first.classList.toggle("active");
+    second.classList.toggle("active");
+    third.classList.toggle("active");
+    about.classList.toggle("active");
 });
